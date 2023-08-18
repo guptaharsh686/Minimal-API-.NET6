7e4c3755c2a6adf6df1412a7464af284179b1764
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-08-18 23:41:56 +0530

    added post put and delete apis

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using MinimalApi.Data;
 using AutoMapper;
 using MinimalApi.Dtos;
+using MinimalApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,5 +33,56 @@ app.MapGet("api/v1/commands", async (ICommandRepo repo, IMapper mapper) =>
     return Results.Ok(mapper.Map<IEnumerable<CommandReadDto>>(commands));
 });
 
+
+app.MapGet("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id) =>
+{
+    var command = await repo.GetCommandById(id);
+    if(command != null)
+    {
+        return Results.Ok(mapper.Map<CommandReadDto>(command));
+    }
+    return Results.NotFound();
+});
+
+app.MapPost("api/v1/commands", async (ICommandRepo repo, IMapper mapper, CommandCreateDto cmdCreateDto) =>
+{
+    var commandModel = mapper.Map<Command>(cmdCreateDto);
+    await repo.CreateCommand(commandModel);
+    await repo.SaveChanges();
+
+    //commandModel will have id since it is reference type id will get allocated after save changes
+    var cmdReadDto = mapper.Map<CommandReadDto>(commandModel);
+
+    return Results.Created($"api/v1/commands/{cmdReadDto.Id}",cmdReadDto);
+});
+
+app.MapPut("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id , CommandUpdateDto cmdUpdateDto) => 
+{
+    var command = await repo.GetCommandById(id);
+    if(command == null) 
+    {
+        return Results.NoContent();
+    }
+
+    mapper.Map(cmdUpdateDto, command);
+
+    await repo.SaveChanges();
+
+    return Results.NoContent(); 
+});
+
+
+app.MapDelete("api/v1/commands/{id}", async (ICommandRepo repo, IMapper mapper, int id) =>
+{
+    var command = await repo.GetCommandById(id);
+    if(command == null)
+    {
+        return Results.NotFound();
+    }
+    repo.DeleteCommand(command);
+    await repo.SaveChanges();
+
+    return Results.NoContent() ;
+});
 
 app.Run();
